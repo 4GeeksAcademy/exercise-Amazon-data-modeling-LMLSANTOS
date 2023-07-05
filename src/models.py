b50df7d6c54d3169ec44e970028f0ee667c70c5c
lmlsantos <126293567+lmlsantos@users.noreply.github.com>
2023-07-05 16:43:59 +0000

    Amazon Data Modeling LMLSANTOS

--- src/models.py
+++ src/models.py
@@ -1,29 +1,61 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import enum
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum, Float
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
+class myEnum(enum.Enum):
+    refund="refund"
+    paid="paid"
+    cancelled="cancelled"
+
+class Product(Base):
+    __tablename__ = 'product'
+    # Here we define columns for the table product
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    pricing = Column(Float)
+    weight = Column(Float)
+    color = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+class Customer(Base):
+    __tablename__ = 'customer'
+    # Here we define columns for the table Customer
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250),unique=True)
+    address = Column(String(250), unique=True)
+   
+class Bill(Base):
+    __tablename__ = 'bill'
+    # Here we define columns for the table Bill
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    created_at = Column(String(250))
+    total_price = Column(Float, nullable=False)
+    status = Column(Enum(myEnum))
+    
+
+class Shopping_Cart(Base):
+    __tablename__ = 'shopping_cart'
+    # Here we define columns for the table Shopping Cart.
+    # Notice that each column is also a normal Python instance attribute.
+    quantity = Column(Integer)
+    price = Column(Float)
+    product_id = Column(Integer, ForeignKey('product.id'),primary_key=True)
+    product = relationship(Product)
+    customer_id = Column(Integer, ForeignKey('customer.id'))
+    customer = relationship(Customer)
+    bill_id = Column(Float, ForeignKey('bill.id'))
+    bill = relationship(Bill)
+
 
     def to_dict(self):
         return {}
